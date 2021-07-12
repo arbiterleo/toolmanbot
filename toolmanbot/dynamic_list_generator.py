ef8734e9a98b11dbcdd2ac9c6ac2dd7ce1249862
william <cilovwx@gmail.com>
2021-07-12 18:50:53 +0800

    your message

--- toolmanbot/dynamic_list_generator.py
+++ toolmanbot/dynamic_list_generator.py
@@ -7,7 +7,7 @@ def favorite_list_generator(favorite_list):
                     content=[
                         TextComponent(text="最愛清單", weight="bold", size="md", margin="sm", wrap=True,),
                         SeparatorComponent(margin = "xl"),
-                        ButtoonComponen(style="primary", color="#997B66", size="md", margin="sm",
+                        ButtonComponent(style="primary", color="#997B66", size="md", margin="sm",
                                         action=MessageAction(label="+", text='請輸入對象名字'), )
                     ])]
     for i in favorite_list:
