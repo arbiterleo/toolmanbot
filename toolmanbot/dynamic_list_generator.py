f44c98488358a48782d573543816ff795b864850
william <cilovwx@gmail.com>
2021-07-13 01:38:06 +0800

    deal with add button

--- toolmanbot/dynamic_list_generator.py
+++ toolmanbot/dynamic_list_generator.py
@@ -4,13 +4,16 @@ def favorite_list_generator(favorite_list):
                     layout="vertical",
                     margin="sm",
                     spacing="sm",
-                    content=[
+                    contents=[
                         TextComponent(text="最愛清單", weight="bold", size="md", margin="sm", wrap=True,),
                         SeparatorComponent(margin = "xl")
-#                        ,ButtonComponent(style="primary", color="#997B66", size="md", margin="sm",
-#                                        action=MessageAction(label="+", text='請輸入對象名字'), )
+                        ,ButtonComponent(style="primary", color="#997B66", size="md", margin="sm",
+                                       action=MessageAction(label="+", text='請輸入對象名字'), )
                     ])]
+
     for i in favorite_list:
+
+
         favorite_button = ButtonComponent(style="primary", color="#997B66", size="sm", margin="sm",
                                         action=MessageAction(label=i, text=f'搜尋對象：{i}'),)
         delete_button=ButtonComponent(style="secondary", color="#F1DCA7", size="sm", margin="sm", flex=0,
@@ -19,11 +22,8 @@ def favorite_list_generator(favorite_list):
                                 contents=[favorite_button, delete_button])
         button_list.append(button_row)
 
-    bubble=BubbleContainer(
-        direction='ltr',
 
-        body=BoxComponent(
-            layout="vertical",
-            contents=button_list
-        )
-    )
+    bubble=BubbleContainer(
+                    director='ltr',
+                    body=BoxComponent(layout="vertical",contents=button_list)
+                    )
