b289ccc3b75c16847c4b76912bb6b938a09172ef
william <cilovwx@gmail.com>
2021-07-14 17:37:53 +0800

    style

--- toolmanbot/dynamic_list_generator.py
+++ toolmanbot/dynamic_list_generator.py
@@ -16,17 +16,17 @@ def favorite_list_generator(favorite_list):
                     margin="sm",
                     spacing="sm",
                     contents=[
-                        TextComponent(text="最愛清單", weight="bold", size="md", margin="sm", wrap=True,),
+                        TextComponent(text="最愛清單", weight="bold", size="xxl", margin="sm", wrap=True,),
                         SeparatorComponent(margin = "xl")
-                        ,ButtonComponent(style="primary", color="#997B66", size="md", margin="sm",
+                        ,ButtonComponent(style="primary", color="#ff66a3", size="md", margin="sm",
                                        action=MessageAction(label="+", text='開始新增對象，請輸入「新增對象：對象名稱」'), )
                     ])]
 
     for i in favorite_list:
 
-        favorite_button = ButtonComponent(style="primary", color="#997B66", size="sm", margin="sm",
+        favorite_button = ButtonComponent(style="primary", color="#ff66a3", size="sm", margin="sm",
                                         action=MessageAction(label=i, text=f'搜尋對象：{i}'),)
-        delete_button=ButtonComponent(style="secondary", color="#F1DCA7", size="sm", margin="sm", flex=0,
+        delete_button=ButtonComponent(style="secondary", color="#ff1a1a", size="sm", margin="sm", flex=0,
                                       action=MessageAction(label="-", text="刪除對象："+i), )
         button_row=BoxComponent(layout="horizontal", margin="md", spacing="sm",
                                 contents=[favorite_button, delete_button])
