b09d6cc9241f726f0928823b1d080ebdb04184c9
william <cilovwx@gmail.com>
2021-08-17 16:54:43 +0800

    image

--- toolmanbot/views.py
+++ toolmanbot/views.py
@@ -31,6 +31,9 @@ from .carousel import carousel_list
 #from .report import draw
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
@@ -56,6 +59,29 @@ topic2=["https://www.youtube.com/channel/UC0C-w0YjGpqDXGB8IHb662A",
 topic3=["https://www.youtube.com/channel/UC0C-w0YjGpqDXGB8IHb662A",
         "https://www.youtube.com/channel/UC0C-w0YjGpqDXGB8IHb662A",
         "https://www.youtube.com/channel/UC0C-w0YjGpqDXGB8IHb662A"]
+
+
+####################
+def draw(CLIENT_ID,attributes):
+
+  # Label
+  labels = np.array(['a', 'b', 'c', 'd', 'e', 'f'])
+  # Value
+  data = np.array(attributes)
+  angles = np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+  data = np.hstack((data, [data[0]]))
+  angles = np.hstack((angles, [angles[0]]))
+  fig = plt.figure(figsize=(10, 10))
+  ax = fig.add_subplot(111, polar=True)
+  ax.plot(angles, data, 'ro-', linewidth=2)
+  plt.savefig('report.png', bbox_inches='tight', pad_inches=0.1)
+
+  PATH='report.png'
+  im = pyimgur.Imgur(CLIENT_ID)
+  uploaded_image = im.upload_image(PATH, title="Report")
+
+  return uploaded_image.link
+#####################
 
 
 @csrf_exempt
