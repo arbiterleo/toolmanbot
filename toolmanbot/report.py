f256427cf5fe6543059f754e76250631cfdce1d4
william <cilovwx@gmail.com>
2021-08-18 21:55:27 +0800

    your_message

--- toolmanbot/report.py
+++ toolmanbot/report.py
@@ -3,25 +3,20 @@ import matplotlib.pyplot as plt
 import pyimgur
 
 def draw(CLIENT_ID,attributes):
+
   # 用於正常顯示中文
   plt.rcParams['font.sans-serif'] = 'SimHei'
   #用於正常顯示符號
   plt.rcParams['axes.unicode_minus'] = False
+  # 構造數據
 
-  plt.style.use('ggplot')
-
-  data = np.array(attributes)
-  feature = [u'對話頻率', u'回話速度', u'回話內容', u'訊息內容量', u'通話頻率', u'通話時間']
+  feature = ['對話頻率', '回話速度', '回話內容', '訊息內容量', '通話頻率', '通話時間']
 
   # 設置每個數據點的顯示位置，在雷達圖上用角度表示
-  angles=np.linspace(0, 2*np.pi,len(data), endpoint=False)
-
-  # 拼接數據首尾，使圖形中線條封閉
-  values=np.concatenate((data,[data[0]]))
-  angles=np.concatenate((angles,[angles[0]]))
+  angles=np.linspace(0, 2*np.pi,len(values), endpoint=False)
 
   # 繪圖
-  fig=plt.figure(figsize=(10, 10))
+  fig=plt.figure()
   # 設置爲極座標格式
   ax = fig.add_subplot(111, polar=True)
   # 繪製折線圖
@@ -36,6 +31,7 @@ def draw(CLIENT_ID,attributes):
   plt.title('好感度報表')
   # 添加網格線
   ax.grid(True)
+
   plt.savefig('report.png', bbox_inches='tight', pad_inches=0.1)
 
   PATH='report.png'
