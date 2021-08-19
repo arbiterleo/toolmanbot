7070a7a6cd807d12ffd4bc484e1c552f3de74c20
william <cilovwx@gmail.com>
2021-08-20 01:50:10 +0800

    close

--- toolmanbot/report.py
+++ toolmanbot/report.py
@@ -2,47 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyimgur
 import cv2
-
-def draw(CLIENT_ID,values,point):
+from matplotlib.gridspec import GridSpec
+def draw(CLIENT_ID,values,point,new_data,pst_data):
 
   #上半部
   img = cv2.imread("./toolmanbot/bg.png")
   # 文字
   text = point
-  cv2.putText(img, text, (273, 210), cv2.FONT_HERSHEY_SIMPLEX,2,(5, 209, 255), 6, cv2.LINE_AA)
+  cv2.putText(img, text, (300, 320), cv2.FONT_HERSHEY_SIMPLEX,4.5,(201, 148, 255), 8, cv2.LINE_AA)
   # 將 BGR 圖片轉為 RGB 圖片
   img = img[:,:,::-1]
   # 繪圖
-  fig=plt.figure(figsize=(8,10))
-  ax1=fig.add_subplot(211)
+  fig=plt.figure(figsize=(8,8))
+
+  gs=GridSpec(2,2)
+
+  rect = fig.patch
+  rect.set_facecolor((0, 0, 0,0))
+
+  ax1=fig.add_subplot(gs[0,0])
   ax1.axis('off')
   ax1.imshow(img)
-
-  #下半部
-  plt.rcParams['axes.unicode_minus'] = False  #用於正常顯示符號
+  #圓餅圖
   feature = ['Frequency', 'Speed', 'Contents', 'Amounts', 'Call frequency', 'Call time']
-  # 設置每個數據點的顯示位置，在雷達圖上用角度表示
-  angles=np.linspace(0, 2*np.pi,len(values), endpoint=False)
-  # 設置爲極座標格式
-  ax = fig.add_subplot(212, polar=True)
 
-  #使圖形封閉
-  values=np.concatenate((values,[values[0]]))
-  angles=np.concatenate((angles,[angles[0]]))
-  feature=np.concatenate((feature,[feature[0]]))
-  # 繪製折線圖
-  ax.plot(angles, values, 'o-', linewidth=2)
-  # 填充顏色
-  ax.fill(angles, values, alpha=0.25)
-  # 設置圖標上的角度劃分刻度，爲每個數據點處添加標籤
-  ax.set_thetagrids(angles * 180/np.pi, feature)
+  ax = fig.add_subplot(gs[0,1])
+  #color = ['#E6CAFF', '#F1E1FF', '#97CBFF', '#ACD6FF','#AAAAFF', '#D3A4FF']
+  color = ['#FFC1E0','#FFD9EC',  '#FF95CA', '#FF60AF','#FF0080', '#FF95CA']
+  ax.pie(values,colors=color,labels = feature[0:6],autopct = "%1.2f%%",pctdistance = 0.6)
+  plt.axis('equal')                                          # 使圓餅圖比例相等
+  #plt.legend(loc = "best")
+  # 添加標題
+  plt.title("Feature's weight Report")
 
+  ax2 = fig.add_subplot(gs[1,:])
+  x = np.arange(0, 6)
 
-  #雷達圖數值分佈
-  ax.set_rlim(-2.0, 2.0)
-  plt.title(u'Favorability Report')
-  # 添加網格線
-  ax.grid(True)
+  ax2.bar(x+0.1, new_data, width = 0.2,color=(1,0,0.5))
+  ax2.bar(x-0.1, pst_data, width = 0.2,color=(1,0.76,0.88))
+  ax2.set_axisbelow(True)
+  ax2.yaxis.grid(color='gray', linestyle='dashed', alpha=0.2)
+  ax2.set_ylim(-100, 100)
+  plt.xticks(x,feature)
+  plt.title("Points (Now v.s Past)")
+  plt.legend(["Now","Past"])
+
 
   #儲存圖片到imgur
   plt.savefig('report.png', bbox_inches='tight', pad_inches=0.1)
