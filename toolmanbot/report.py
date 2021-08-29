66966a2e09ab7ca2100a531d9f6256b1e2d051c1
william <cilovwx@gmail.com>
2021-08-29 23:11:31 +0800

    text

--- toolmanbot/report.py
+++ toolmanbot/report.py
@@ -41,9 +41,9 @@ def draw(CLIENT_ID,values,point,difference):
     if max(values1)==values1[0]:
         imageA = cv2.imread('./toolmanbot/unicorn.png')
         text = point
-        cv2.putText(imageA, text, (17, 195), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,1.75,(0, 0, 0), 4, cv2.LINE_AA)
+        cv2.putText(imageA, text, (17, 195), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,2,(0, 0, 0), 4, cv2.LINE_AA)
         text1 = difference
-        cv2.putText(imageA, text1, (135, 190), cv2.FONT_HERSHEY_COMPLEX,0.55,(22, 116, 226), 1, cv2.LINE_AA)
+        cv2.putText(imageA, text1, (145, 200), cv2.FONT_HERSHEY_COMPLEX,0.55,(22, 116, 226), 1, cv2.LINE_AA)
         imageA = Image.fromarray(cv2.cvtColor(imageA,cv2.COLOR_BGR2RGB))
         imageA = imageA.convert('RGBA')
         widthA , heightA = imageA.size
