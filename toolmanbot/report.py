a388a213d6ff1f0660b76ddcb41eb95a0986b203
william <cilovwx@gmail.com>
2021-09-02 00:31:27 +0800

    text

--- toolmanbot/report.py
+++ toolmanbot/report.py
@@ -80,7 +80,7 @@ def draw(CLIENT_ID,values,values_a,point,difference):
     plt.polar(theta,values_a,color="#7E7E7E",marker='.')
     plt.xticks(theta,features)
     plt.fill(theta,values,color="#DD2D57",alpha=0.5)
-    plt.fill(theta,values_a,color="#C7C7C7",alpha=0.5)
+    #plt.fill(theta,values_a,color="#C7C7C7",alpha=0.5)
     plt.savefig('report.png', bbox_inches='tight', pad_inches=0.1,transparent = True)
     PATH='report.png'
     im = pyimgur.Imgur("e00f48cb1956755")
